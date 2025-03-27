781849b366d36eb069cededae58fe6d1339c40bc
Reinami <tyhonnge@gmail.com>
2025-03-27 12:01:02 -0700

    es

--- scripts/bash.sh
+++ scripts/bash.sh
@@ -24,8 +24,8 @@ log_note "Starting $script_name..."
 case "$OS_TYPE" in
     pop)
         cp git/.git-prompt.sh ~/
-        local git_prompt_line="PROMPT_COMMAND='PS1_CMD1=\$(__git_ps1 \" (%s)\")'; PS1='[\\[\\e[32m\\]\\u\\[\\e[0m\\]@\\[\\e[95m\\]\\h\\[\\e[0m\\] \\[\\e[96m\\]\\w\\[\\e[0m\\]]\\[\\e[90m\\]\${PS1_CMD1}\\[\\e[0m\\] \\[\\e[91m\\]λ\\[\\e[0m\\] '"
-        local git_source_line="source ~/.git-prompt.sh"
+        git_prompt_line="PROMPT_COMMAND='PS1_CMD1=\$(__git_ps1 \" (%s)\")'; PS1='[\\[\\e[32m\\]\\u\\[\\e[0m\\]@\\[\\e[95m\\]\\h\\[\\e[0m\\] \\[\\e[96m\\]\\w\\[\\e[0m\\]]\\[\\e[90m\\]\${PS1_CMD1}\\[\\e[0m\\] \\[\\e[91m\\]λ\\[\\e[0m\\] '"
+        git_source_line="source ~/.git-prompt.sh"
 
         append_to_bashrc "$git_prompt_line"
         append_to_bashrc "$git_source_line"
