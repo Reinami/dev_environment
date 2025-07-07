aa5404bc631a1bc081d77668c5744d1ba48b1b62
Reinami <13673948+Reinami@users.noreply.github.com>
2025-07-06 22:42:55 -0700

    Update telescope.lua

--- neovim/.config/nvim/lua/reinami/lazy/telescope.lua
+++ neovim/.config/nvim/lua/reinami/lazy/telescope.lua
@@ -5,10 +5,8 @@ return {
         local builtin = require("telescope.builtin")
 
         -- Key mappings
-        vim.keymap.set("n", "<C-f>", builtin.find_files, {})
+        vim.keymap.set("n", "<C-s>", builtin.find_files, {})
         vim.keymap.set("n", "<C-g>", builtin.git_files, {})
-        vim.keymap.set("n", "<C-s>", function()
-            builtin.grep_string({ search = vim.fn.input("Grep > ") })
-        end)
+        vim.keymap.set("n", "<C-f>", builtin.live_grep, {})
     end,
 }
